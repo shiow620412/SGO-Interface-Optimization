43fbf687c7e20e235a973af957a9e57f26402f39
Wind-PC <shiow620412@gmail.com>
2023-05-26 17:42:01 +0800

    因應插件重構 update release script

--- release.py
+++ release.py
@@ -1,21 +1,47 @@
-import re
+import os
+import shutil
+import subprocess
+# subprocess.call("npm run test", shell=True)
 
-file = "sgoScript.js"
-f = open(file, "r", encoding="utf-8")
+# file = "./dist/main.js"
+globalVarsFile = "./src/storage/globalVars.js"
+print(f"讀取{globalVarsFile}...")
+
+f = open(globalVarsFile, "r+", encoding="utf-8")
 lines = f.readlines()
 f.close()
-print(f"讀取{file}...")
-print(f"原始版本號:{lines[3][17:]}", end="")
+currentVersion = lines[1][14:20]
+print(f"當前版本號:{currentVersion}")
 newVersion = input("新版本號:")
-lines[3] = re.sub("1.[0-9]+.[0-9]+", newVersion, lines[3])
-lines[15] = re.sub("1.[0-9]+.[0-9]+", newVersion, lines[15])
-# for index in range(lines.__len__()):
-#     lines[index] = re.sub("1.[0-9]+.[0-9]+", "1.18.0", lines[index])
-
-releaseFile = open(f"release\sgoScript Ver{newVersion}.js", "w+", encoding="utf-8")
-releaseFile.writelines(lines)
-releaseFile.close()
-print(f"輸出檔案:release\\sgoScript Ver{newVersion}.js")
-f = open(file, "w+", encoding="utf-8")
+lines[1] = lines[1].replace(currentVersion, newVersion)
+f = open(globalVarsFile, "w+", encoding="utf-8")
 f.writelines(lines)
 f.close()
+
+
+subprocess.call("npm run test", shell=True)
+
+file = "./dist/main.js"
+f = open(file, "r+", encoding="utf-8")
+codes = f.readlines()
+f.close()
+
+outputFile = open(f"./dist/main_tampermonkey.js", "w+", encoding="utf-8")
+outputFile.write(f"""// ==UserScript==
+// @name         Sword Gale Online 介面優化
+// @namespace    http://tampermonkey.net/
+// @version      {newVersion}
+// @description  優化界面
+// @author       Wind
+// @match        https://swordgale.online/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=swordgale.online
+// @grant        none
+// @license MIT
+// @run-at document-start
+// ==/UserScript==
+""")
+outputFile.writelines(codes)
+outputFile.close()
+shutil.copy("./dist/main_tampermonkey.js", f"./release/sgoScript Ver{newVersion}.js")
+print(f"輸出檔案:./dist/main_tampermonkey.js")
+print(f"輸出檔案:./release/sgoScript Ver{newVersion}.js")
